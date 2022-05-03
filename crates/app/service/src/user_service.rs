94c5b367454a149e87dca089d20e1a09f33ccd94
Iacopo Melani <iacopomelani4@gmail.com>
2022-05-03 22:32:11 +0200

    app/service: fix UserService on Context and refs

--- crates/app/service/src/user_service.rs
+++ crates/app/service/src/user_service.rs
@@ -1,20 +1,21 @@
-use openmusicgang_common::context::Context;
+use openmusicgang_common::context::AppContext;
 use openmusicgang_common::error::Error;
 use openmusicgang_entity::user::User;
 
 /// UserService is the service for user management.
 pub trait UserService {
-    fn create_user(&self, ctx: Context, user: User) -> Result<(), Error>;
+    fn create_user(&self, ctx: AppContext, user: &mut User) -> Result<(), Error>;
 
-    fn delete_user(&self, ctx: Context, id: u64) -> Result<(), Error>;
+    fn delete_user(&self, ctx: AppContext, id: u64) -> Result<(), Error>;
 
-    fn update_user(&self, ctx: Context, user: UserUpdate) -> Result<(), Error>;
+    fn update_user(&self, ctx: AppContext, user: UserUpdate) -> Result<(), Error>;
 
-    fn find_user_by_id(&self, ctx: Context, id: u64) -> Result<User, Error>;
+    fn find_user_by_id(&self, ctx: AppContext, id: u64) -> Result<User, Error>;
 
-    fn find_user_by_email(&self, ctx: Context, email: String) -> Result<User, Error>;
+    fn find_user_by_email(&self, ctx: AppContext, email: String) -> Result<User, Error>;
 
-    fn find_users(&self, ctx: Context, filters: UserFilter) -> Result<(Vec<User>, usize), Error>;
+    fn find_users(&self, ctx: AppContext, filters: UserFilter)
+        -> Result<(Vec<User>, usize), Error>;
 }
 
 /// UserUpdate is a struct for allowed fields to update a user.
--- crates/mock/src/user.rs
+++ crates/mock/src/user.rs
@@ -1,4 +1,4 @@
-use openmusicgang_common::context::Context;
+use openmusicgang_common::context::AppContext;
 use openmusicgang_common::error::Error;
 use openmusicgang_entity::user::User;
 use openmusicgang_service::user_service::{
@@ -6,51 +6,55 @@ use openmusicgang_service::user_service::{
 };
 
 pub struct UserService {
-    pub create_user_fn: Option<fn(Context, User) -> Result<(), Error>>,
-    pub delete_user_fn: Option<fn(Context, u64) -> Result<(), Error>>,
-    pub update_user_fn: Option<fn(Context, UserUpdate) -> Result<(), Error>>,
-    pub find_user_by_id_fn: Option<fn(Context, u64) -> Result<User, Error>>,
-    pub find_user_by_email_fn: Option<fn(Context, String) -> Result<User, Error>>,
-    pub find_users_fn: Option<fn(Context, UserFilter) -> Result<(Vec<User>, usize), Error>>,
+    pub create_user_fn: Option<fn(AppContext, &mut User) -> Result<(), Error>>,
+    pub delete_user_fn: Option<fn(AppContext, u64) -> Result<(), Error>>,
+    pub update_user_fn: Option<fn(AppContext, UserUpdate) -> Result<(), Error>>,
+    pub find_user_by_id_fn: Option<fn(AppContext, u64) -> Result<User, Error>>,
+    pub find_user_by_email_fn: Option<fn(AppContext, String) -> Result<User, Error>>,
+    pub find_users_fn: Option<fn(AppContext, UserFilter) -> Result<(Vec<User>, usize), Error>>,
 }
 
 impl UserServiceTrait for UserService {
-    fn create_user(&self, ctx: Context, user: User) -> Result<(), Error> {
+    fn create_user(&self, ctx: AppContext, user: &mut User) -> Result<(), Error> {
         if let Some(f) = self.create_user_fn {
             return f(ctx, user);
         }
         panic!("create_user_fn not set");
     }
 
-    fn delete_user(&self, ctx: Context, id: u64) -> Result<(), Error> {
+    fn delete_user(&self, ctx: AppContext, id: u64) -> Result<(), Error> {
         if let Some(f) = self.delete_user_fn {
             return f(ctx, id);
         }
         panic!("delete_user_fn not set");
     }
 
-    fn update_user(&self, ctx: Context, user: UserUpdate) -> Result<(), Error> {
+    fn update_user(&self, ctx: AppContext, user: UserUpdate) -> Result<(), Error> {
         if let Some(f) = self.update_user_fn {
             return f(ctx, user);
         }
         panic!("update_user_fn not set");
     }
 
-    fn find_user_by_id(&self, ctx: Context, id: u64) -> Result<User, Error> {
+    fn find_user_by_id(&self, ctx: AppContext, id: u64) -> Result<User, Error> {
         if let Some(f) = self.find_user_by_id_fn {
             return f(ctx, id);
         }
         panic!("find_user_by_id_fn not set");
     }
 
-    fn find_user_by_email(&self, ctx: Context, email: String) -> Result<User, Error> {
+    fn find_user_by_email(&self, ctx: AppContext, email: String) -> Result<User, Error> {
         if let Some(f) = self.find_user_by_email_fn {
             return f(ctx, email);
         }
         panic!("find_user_by_email_fn not set");
     }
 
-    fn find_users(&self, ctx: Context, filters: UserFilter) -> Result<(Vec<User>, usize), Error> {
+    fn find_users(
+        &self,
+        ctx: AppContext,
+        filters: UserFilter,
+    ) -> Result<(Vec<User>, usize), Error> {
         if let Some(f) = self.find_users_fn {
             return f(ctx, filters);
         }
